d7e16de0b4960e173ad21c3fdef0ab62068700d8
yiannisha <hadjiyiannis.3@gmail.com>
2021-09-14 18:36:16 +0300

    Make draft of AmazonScrape.py

---
+++ AmazonScrape.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from scrapy_backend.scrapy_backend.spiders.amazon_spider import AmazonSpider
+
+def get_categories():
+    '''
+    Returns a dictionary with numbers as keys, categories as values -> {1 : 'fashion',...}
+    '''
+    pass
+
+if __name__ == '__main__':
+
+    categories = get_categories()
+
+    # default values
+
+    # read input
+
+    # create a CrawlerProcess
+    process = CrawlerProcess(get_project_settings())
+
+    # set AmazonSpider to crawl with given start_urls
+    process.crawl(AmazonSpider, start_urls=[])
+
+    # begin crawling
+    process.start()
